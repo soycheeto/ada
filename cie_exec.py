7a7abf8cf36876410a7faff82c68fc19dff340e8
Trisha D'Souza <soycheeto@Trishas-MacBook-Air.local>
2025-04-16 11:45:08 +0530

    added time complexity

--- cie_exec.py
+++ cie_exec.py
@@ -1,7 +1,9 @@
+import time
 n = int(input("Enter the value count: "))
 print("Enter the elements individually: ")
 arr = [int(input()) for _ in range(n)]
 target = int(input("Enter the search value: "))
+start = time.time()
 for i in range(n):
     if arr[i]==target: 
         print(f"Item found at index {i}")
@@ -9,3 +11,5 @@ for i in range(n):
         break
 if not found:
     print("Item not found.")
+end = time.time()
+print(f"Time taken to find the target = {end-start} seconds.")
